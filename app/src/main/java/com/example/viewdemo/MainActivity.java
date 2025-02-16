2cfe034c4c3cae0365cc7ef430184558524a53a2
Pukarojha <0zha.pukar@gmail.com>
2025-02-15 23:05:56 -0800

    Jan 30 rec 30 min

--- app/src/main/java/com/example/viewdemo/MainActivity.java
+++ app/src/main/java/com/example/viewdemo/MainActivity.java
@@ -15,6 +15,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imgViewSample ;
@@ -47,11 +49,27 @@ public class MainActivity extends AppCompatActivity {
         //getResources(): retrives the resources associated with (like Strings, colors, etc)
         //getTheme(): gets the theme of current activity
 
-        img.setBounds(0,0,80,80);
+        Objects.requireNonNull(img).setBounds(0,0,80,80);
+        //the setbounds() throws null pointer exception if there is no image or null value
+        //therefore, Objects.requireNonNUll(img) is used to eradicate exception occurance.
 
         //setBounds(0,0,80,80); here means:
         //L, T is 0, 0, R, B is 80, 80
         //which basically means draw an image from left, top 0, 0 to Right 80, Bottom 80
         //which simplifies to width of 80 and height of 80
+
+        img.setBounds(0,0, img.getIntrinsicWidth(), img.getIntrinsicHeight());
+        //here are the two setBounds, which defines as:
+        // first setBounds() for specifying the fix size for image.
+        //it says image should be drawn within the size of width: 80 and height: 80
+
+        // the second setBounds(): sets the image bounds to actual size of image itself
+        txtViewSample.setCompoundDrawables(img, null, img, null);
+
+        //setCompoundDrawables() is a method that allows you to set images around a TextView.
+        // You can place images to the left, top, right, or bottom of the text in the TextView
+
+        txtViewSample.setCompoundDrawablePadding(10);
+        txtViewSample.setAlpha(1f);  // 1f -> fully opaque 0f- transparent
     }
 }
